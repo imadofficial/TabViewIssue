e05fa5375545144b8e11628cfab41ce6b8857178
imadofficial <imadamroug89@outlook.com>
2024-07-01 12:38:38 +0200

    Initial commit

---
+++ TabViewIssue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TabViewIssue;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TabViewIssue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TabViewIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
